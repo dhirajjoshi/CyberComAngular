6222680f7c8c290a4e64cf7a7e12f4693688ff21
Dhiraj joshi <56167665+dhirajjoshi@users.noreply.github.com>
2022-02-17 12:15:45 +0530

    Test modify again

--- first/src/app/module/test/components/danger/danger.component.ts
+++ first/src/app/module/test/components/danger/danger.component.ts
@@ -3,10 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-danger',
   templateUrl: './danger.component.html',
-  styles: [`p {
-    background-color: #ff5c5c;
-    color: maroon;
-}`]
+  styleUrls: [`./danger.component.css`]
 })
 export class DangerComponent {
 
--- first/src/app/module/test/components/warning/warning.component.ts
+++ first/src/app/module/test/components/warning/warning.component.ts
@@ -3,10 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-warning',
   templateUrl: './warning.component.html',
-  styles: [`p {
-    background-color: #feeb75;
-    color: black;
-}`]
+  styleUrls: ['./warning.component.css']
 })
 export class WarningComponent  {
 
